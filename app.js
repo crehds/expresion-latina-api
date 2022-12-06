469c378d0dcd8974ed5404b03426c51fd0bf2c6e
Carlos Rodriguez <carlos15erh@gmail.com>
2022-12-06 17:38:10 -0500

    feat: support all cors request

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -8,6 +9,7 @@ const postersRouter = require('./routes/api/posters');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
